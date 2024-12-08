e2bc2a4c922835ade72d5f3e4b88ebd51aafb1cf
Cheulong Sear <searcheulong@gmail.com>
2024-12-09 01:14:26 +0700

    07_loops

---
+++ bash/basic/07_loops.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+for i in {1..10}; do
+    echo "Number $i"
+done
+
+# for i in {1..10}; do
+#     echo "Number $i"
+#     if [ "$i" -eq 5 ]; then
+#         break
+#     fi
+# done
+
+# for i in {1..10}; do
+#     echo "Number $i"
+#     if [ "$i" -eq 5 ]; then
+#         break
+#     fi
+# done
+
+# # while loop
+# i=1
+# while [ "$i" -le 10 ]; do
+#     echo "Number $i"
+#     i=$((i + 1))
+# done
+
+# # until loop
+# i=1
+# until [ "$i" -gt 10 ]; do
+#     echo "Number $i"
+#     i=$((i + 1))            
+# done
