919027efc76ba8ee8fb4ebb184079c4e13debc4e
Cheulong Sear <searcheulong@gmail.com>
2024-12-09 00:38:05 +0700

    05_file-condition

---
+++ bash/basic/05_file-condition.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+FILE="test.txt"
+
+if [ -f "$FILE" ]; then
+    echo "$FILE exists"
+else
+    echo "$FILE does not exist"
+fi
+
+########
+# -d file   True if the file is a directory
+# -e file   True if the file exists (note that this is not particularly portable, thus -f is generally used)
+# -f file   True if the provided string is a file
+# -g file   True if the group id is set on a file
+# -r file   True if the file is readable
+# -s file   True if the file has a non-zero size
+# -u    True if the user id is set on a file
+# -w    True if the file is writable
+# -x    True if the file is an executable
+########
