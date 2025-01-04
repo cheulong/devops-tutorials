8bc750e00c0f45244a60b59fa2830f7e94bbae77
Cheulong Sear <searcheulong@gmail.com>
2025-01-04 21:13:42 +0700

    update python print format

--- python/basic/01_print.py
+++ python/basic/01_print.py
@@ -2,3 +2,7 @@ print("Hello World!")
 print(1+3)
 print("hello, my name is \"Jay\", nice to meet you")
 print('Sue said "I don\'t like Pizza"')
+age = 7
+height = 160
+weight = 50
+print (f"So, you're {age} old, {height} tall and {weight} heavy.")
