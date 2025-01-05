bdc022f9e5dae7316cd9934b02e2c64eee6f581c
Cheulong Sear <searcheulong@gmail.com>
2025-01-05 20:48:35 +0700

    list and dic

---
+++ python/basic/04_list_dic.py
@@ -0,0 +1,37 @@
+turtles = ["turtle1", "turtle2", "turtle3"]
+turtles.append("turtle4")
+
+for turtle in turtles:
+    print(turtle)
+
+# turtles[0] = "turtle1"
+# turtles[1] = "turtle2"
+# turtles[2] = "turtle3"
+
+# turtles.pop(0)
+# turtles.pop(1)
+# turtles.pop(2)
+
+# turtles.remove("turtle1")
+# turtles.remove("turtle2")
+# turtles.remove("turtle3")
+
+# turtles.insert(0, "turtle1")
+# turtles.insert(1, "turtle2")
+# turtles.insert(2, "turtle3")
+
+# turtles.sort()
+
+# turtles.reverse()
+
+turtles = {"turtle1": 1, "turtle2": 2, "turtle3": 3}
+
+for turtle in turtles:
+    print(turtle)
+
+# turtles["turtle4"] = 4
+
+# del turtles["turtle1"]
+# del turtles["turtle2"]
+# del turtles["turtle3"]
+
