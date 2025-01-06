70c28e528b1753d2263f243b09f6b713268fc51b
Cheulong Sear <searcheulong@gmail.com>
2025-01-07 05:49:41 +0700

    convert data type

--- python/basic/02_type.py
+++ python/basic/02_type.py
@@ -3,3 +3,10 @@ print(type(1+3))
 print(type(10/3))
 print(type(10//3))
 print(10//3)
+
+# Converting to data type
+print(int(10/3))
+print(float(10/3))
+print(str(10/3))
+print(bool(10/3))
+print(int('10'))
