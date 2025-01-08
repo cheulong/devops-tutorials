4f336f868a960c6ff687cc32b32ad9ff79e2b29d
Cheulong Sear <searcheulong@gmail.com>
2025-01-09 05:25:30 +0700

    function

---
+++ python/basic/06_function.py
@@ -0,0 +1,12 @@
+def my_function():
+    """This is a function
+    This is a function
+    This is a function
+    """
+    global mynum1
+    mynum1 = 10
+    print(mynum1)
+
+my_function()
+
+print(mynum1)
