0000548481da8dfd2c8eb623d95cd490b76a134f
Cheulong Sear <searcheulong@gmail.com>
2025-01-08 21:37:18 +0700

    string method

--- python/basic/03_variables.py
+++ python/basic/03_variables.py
@@ -3,3 +3,25 @@ print(my_var)
 my_var_1 = "Hello"
 my_var_2 = "World"
 print(my_var_1 + " " + my_var_2)
+
+# String functions
+my_str = "This is a string"
+print(my_str[3])
+print(my_str.upper())
+print(my_str.lower())
+
+# String methods
+my_str = "This is a string"
+print(my_str.startswith("This"))
+print(my_str.endswith("string"))
+
+# String slicing
+my_str = "This is a string"
+print(my_str[0:5])
+print(my_str[5:])
+
+# String concatenation
+my_str = "This is a string"
+my_str_2 = " and another string"
+print(my_str + my_str_2)
+print("This is a \nstring")
