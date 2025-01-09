3c67b5b6125d33cca3c481b39ec0b4fbb35b2a80
Cheulong Sear <searcheulong@gmail.com>
2025-01-09 07:44:29 +0700

    loop

---
+++ python/basic/09_loop.py
@@ -0,0 +1,12 @@
+my_num = 0
+
+while my_num < 10:
+    print(my_num)
+    my_num = my_num + 1
+
+for my_num in range(10):
+    print(my_num)
+
+foods = ["apple", "banana", "cherry"]
+for food in foods:
+    print(food)
