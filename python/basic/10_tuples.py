7bdd14d5c8de7e12be337fe7881b670ae8d4eaa1
Cheulong Sear <searcheulong@gmail.com>
2025-01-09 07:51:17 +0700

    tuples

---
+++ python/basic/10_tuples.py
@@ -0,0 +1,10 @@
+# Tuples
+my_tuple = ("Hello", "World")
+print(my_tuple)
+print(my_tuple[0])
+print(my_tuple[1])
+print(len(my_tuple))
+
+# Tuples are immutable
+# my_tuple[0] = "Hello"
+# print(my_tuple)
